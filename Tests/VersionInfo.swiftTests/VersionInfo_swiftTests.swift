60f3024ee72a0d661fbb7b281ce14421eb16bc5d
Adam Laszlo Rocska <adam-rocska@users.noreply.github.com>
2024-06-07 17:26:12 +0200

    🏁 Swift package init

---
+++ Tests/VersionInfo.swiftTests/VersionInfo_swiftTests.swift
@@ -0,0 +1,12 @@
+import XCTest
+@testable import VersionInfo_swift
+
+final class VersionInfo_swiftTests: XCTestCase {
+    func testExample() throws {
+        // XCTest Documentation
+        // https://developer.apple.com/documentation/xctest
+
+        // Defining Test Cases and Test Methods
+        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+    }
+}
