3d62151e9ea4d6ef16e89a3c5c6a1c7261f7a3f7
Adam Laszlo Rocska <adam-rocska@users.noreply.github.com>
2026-05-13 17:05:20 +0200

    ⬆️ First steps for a swift upgrade.

--- Package.swift
+++ Package.swift
@@ -1,17 +1,22 @@
-// swift-tools-version: 5.10
+// swift-tools-version: 6.0
 
 import PackageDescription
 
 let package = Package(
   name: "VersionInfo.swift",
-  platforms: [.macOS(.v14), .iOS(.v16), .watchOS(.v9), .tvOS(.v17), .visionOS(.v1)],
+  platforms: [
+    .macOS(.v14), .iOS(.v16), .watchOS(.v9), .tvOS(.v17), .visionOS(.v1),
+  ],
   products: [
     .plugin(name: "VersionInfoPlugin", targets: ["VersionInfoPlugin"]),
     .library(name: "VersionInfo", targets: ["VersionInfo"]),
     .executable(name: "version-info-gen", targets: ["version-info-gen"]),
   ],
   dependencies: [
-    .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.4.0")
+    .package(
+      url: "https://github.com/apple/swift-argument-parser.git",
+      from: "1.4.0"
+    )
   ],
   targets: [
     .target(name: "VersionInfo"),
@@ -22,7 +27,9 @@ let package = Package(
     ),
     .executableTarget(
       name: "version-info-gen",
-      dependencies: [.product(name: "ArgumentParser", package: "swift-argument-parser")]
+      dependencies: [
+        .product(name: "ArgumentParser", package: "swift-argument-parser")
+      ]
     ),
   ]
 )
